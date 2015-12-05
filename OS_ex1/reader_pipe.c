7434ecef370df1c0c9581d5b601f1f812524abd1
guysalama <guysalama@mail.tau.ac.il>
2015-12-06 00:30:42 +0200

    reader

--- OS_ex1/reader_pipe.c
+++ OS_ex1/reader_pipe.c
@@ -14,14 +14,11 @@
 #define ARG_ERROR "The given path %s is not valid: %s\n"
 #define FUNC_ERROR "Error occurred while running the function %s: %s\n"
 #define FIFO_ERROR "The given file in path %s isn't a FIFO file\n"
-#define BUF_SIZE 4096
-
-// Globals
-char read_buf[BUF_SIZE];
-
+#define LINE_SIZE 1024
+//#define BUF_SIZE 4096
 
 int read_from_fifo(int fd);
-int print_chars(int len);
+//int print_chars(int len);
 
 int main(int argc, char** argv){
 	int fd, ret_val = 0;
@@ -32,16 +29,20 @@ int main(int argc, char** argv){
 	}
 	struct stat st;
 	while (1){
-		while ((stat(argv[1], &st) == -1) && (errno == ENOENT)) sleep(1);
-		if (stat(argv[1], &st) == -1){
+		if ((stat(argv[1], &st) == -1) && (errno != ENOENT)){
 			printf(ARG_ERROR, argv[1], strerror(errno));
 			return -1;
 		}
+		while ((stat(argv[1], &st) == -1) && (errno == ENOENT)) sleep(1);
 		if (!S_ISFIFO(st.st_mode)){ // It isn't fifo file
 			printf(FIFO_ERROR, argv[1]);
 			return -1;
 		}
 		fd = open(argv[1], O_WRONLY);
+		if (fd == -1){
+			printf(FUNC_ERROR, "open", strerror(errno));
+			return -1;
+		}
 		if (read_from_fifo(fd) == -1){
 			close(fd);
 			return -1;
@@ -51,32 +52,48 @@ int main(int argc, char** argv){
 }
 
 int read_from_fifo(int fd){
-	size_t len, total_len;
-	read_buf[BUF_SIZE] = '\0';
-	while (0){
-		len = read(fd, read_buf, BUF_SIZE);
-		if (len == -1) return -1;
-		if (len == BUF_SIZE){
-			total_len += len;
-			lseek(fd, total_len, SEEK_SET);
-			print_chars(len);
-			continue;
+	int num;
+	char line[LINE_SIZE];
+	do{
+		num = read(fd, line, LINE_SIZE);
+		if (read == -1){
+			printf(FUNC_ERROR, "read", strerror(errno));
+			return -1;
 		}
-		if (len != BUF_SIZE){
-			read_buf[len] = '\0';
-			print_chars(len);
-			break;
-		}
-	}
+		line[num] = '\0';
+		printf("%s", line);
+	} while (num > 0);
 	return 0;
 }
 
-
-int print_chars(int len){
-	int i;
-	for (i = 0; i <= len; i++){
-		printf("%c", read_buf[i]);
-	}
-}
-
-
+//
+//int read_from_fifo(int fd){
+//	size_t len, total_len;
+//	read_buf[BUF_SIZE] = '\0';
+//	while (0){
+//		len = read(fd, read_buf, BUF_SIZE);
+//		if (len == -1) return -1;
+//		if (len == BUF_SIZE){
+//			total_len += len;
+//			lseek(fd, total_len, SEEK_SET);
+//			print_chars(len);
+//			continue;
+//		}
+//		if (len != BUF_SIZE){
+//			read_buf[len] = '\0';
+//			print_chars(len);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+//
+//
+//int print_chars(int len){
+//	int i;
+//	for (i = 0; i <= len; i++){
+//		printf("%c", read_buf[i]);
+//	}
+//}
+//
+//
--- OS_ex1/ref2_2.c
+++ OS_ex1/ref2_2.c
@@ -1,174 +1,99 @@
-// usage example:
-// sudo ./raid0 /dev/sdb /dev/sdc
-
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #include <sys/types.h>
-#include <fcntl.h> // for open flags
+#include <fcntl.h>
 #include <assert.h>
-#include <errno.h> 
 #include <string.h>
+#include <errno.h>
+#include <signal.h>
 
-#define DEVICE_SIZE (1024*1024*256) // assume all devices identical in size
+int fifo;
+char* f_path;
 
-#define SECTOR_SIZE 512
-#define SECTORS_PER_BLOCK 4
-#define BLOCK_SIZE (SECTOR_SIZE * SECTORS_PER_BLOCK)
-
-#define BUFFER_SIZE (BLOCK_SIZE * 2)
-
-char	buf[BUFFER_SIZE];
-int		num_dev;
-int		num_raid0_dev;
-int		num_raid1_dev;
-int		*dev_fd;
-
-void do_raid0_rw(char* operation, int sector, int count)
-{
-	int i = sector;
-
-	while (i < sector + count)
-	{
-		// find the relevant device for current sector
-		int block_num = i / SECTORS_PER_BLOCK;
-		int dev_num = block_num % num_raid0_dev;
-
-		// find offset of sector inside device
-		int block_start = i / (num_raid0_dev * SECTORS_PER_BLOCK);
-		int block_off = i % SECTORS_PER_BLOCK;
-		int sector_start = block_start * SECTORS_PER_BLOCK + block_off;
-		int offset = sector_start * SECTOR_SIZE;
-
-		// try to write few sectors at once
-		int num_sectors = SECTORS_PER_BLOCK - block_off;
-		while (i + num_sectors > sector + count)
-			--num_sectors;
-		int sector_end = sector_start + num_sectors - 1;
-		int size = num_sectors * SECTOR_SIZE;
-
-		// validate calculations
-		assert(num_sectors > 0);
-		assert(size <= BUFFER_SIZE);
-		assert(offset + size <= DEVICE_SIZE);
-
-		do_raid1_rw(operation, dev_num, offset, size, sector_start, sector_end);
-
-		i += num_sectors;
+static void signal_handler(int signum){
+	if (signum == SIGINT || signum == SIGTERM){
+		if (unlink(f_path) != 0) printf("Error removing FIFO: %s\n", strerror(errno));
+		if (close(fifo) != 0) printf("Error closing FIFO: %s\n", strerror(errno));
+		exit(0);
 	}
+	if (signum == SIGPIPE) printf("Error: writing to FIFO without open reader\n");
 }
 
-int do_raid1_rw(char* operation, int dev_num, int offset, int size, int sector_start, int sector_end){
-	int i = 0;
-	while (dev_fd[dev_num + i] < 0 && i < num_raid1_dev) i++;
-	if (i == num_raid1_dev){
-		printf("Operation on bad device %d\n", dev_num);
+int reg_signals(){
+	struct sigaction signal;
+	signal.sa_handler = &signal_handler;
+	sigemptyset(&signal.sa_mask);
+	signal.sa_flags = 0;
+
+	if (sigaction(SIGTERM, &signal, NULL) < 0) {
+		printf("Error registering signal handler for SIGTERM: %s\n", strerror(errno));
 		return 0;
 	}
-	if (!strcmp(operation, "READ")){
-		assert(offset == lseek(dev_fd[dev_num + i], offset, SEEK_SET));
-		assert(size == read(dev_fd[dev_num + i], buf, size));
-		printf("Operation on device %d, sector %d-%d\n",
-			dev_num + i, sector_start, sector_end);
-	}
-	else if (!strcmp(operation, "WRITE")){
-		for (i; i < num_raid1_dev; i++){
-			if (dev_fd[dev_num + i] > 0){
-				assert(offset == lseek(dev_fd[dev_num + i], offset, SEEK_SET));
-				assert(size == write(dev_fd[dev_num + i], buf, size));
-				printf("Operation on device %d, sector %d-%d\n",
-					dev_num + i, sector_start, sector_end);
-			}
-		}
+
+	if (sigaction(SIGINT, &signal, NULL) < 0) {
+		printf("Error registering signal handler for SIGINT: %s\n", strerror(errno));
+		return 0;
 	}
 
+	if (sigaction(SIGPIPE, &signal, NULL) < 0) {
+		printf("Error registering signal handler for SIGPIPE: %s\n", strerror(errno));
+		return 0;
+	}
+	return 1;
 }
 
-int restore_data(int dev){
-	int raid0_dev = dev / num_raid1_dev;
-	int i = 0;
-	ssize_t read_b, written_b;
-	for (i = 0; i < num_raid1_dev; i++){
-		if ((raid0_dev + i) != dev && dev_fd[raid0_dev + i] > 0){
-			// continue from where we stopped
-			lseek(dev_fd[raid0_dev + i], lseek(dev_fd[dev], 0, SEEK_CUR), SEEK_SET);
-			// dup content
-			read_b = read(dev_fd[raid0_dev + i], buf, BUFFER_SIZE)
-				while (read_b > 0){
-				if (read_b != write(dev_fd[dev], buf, read_b)){
-					// kill new dev and return err
-					assert(!close(dev_fd[dev]));
-					dev_fd[dev] = -1;
-					return 0;
-				}
-				read_b = read(dev_fd[raid0_dev + i], buf, BUFFER_SIZE)
-				}
-			if (read_b != 0){
-				// error in dev copied from, kill and continue to next
-				assert(!close(dev_fd[raid0_dev + i]));
-				dev_fd[raid0_dev + i] = -1;
+int open_write_fifo(char* path){
+	struct stat statbuf;
+	int fd;
+	if (stat(path, &statbuf) == 0){
+		// file exists
+		if (S_ISFIFO(statbuf.st_mode)){
+			if ((fd = open(path, O_WRONLY)) == -1){
+				printf("Error opening FIFO: %s : %s\n", path, strerror(errno));
+				return 0;
 			}
-			else return 1; // got to EOF
+			return fd;
+		}
+		int retval = unlink(path);
+		assert(retval == 0);
+	}
+	else{
+		if (errno != ENOENT){ // file exists but got other error from stat
+			printf("file exists, got error %s\n", strerror(errno));
+			return 0;
 		}
 	}
-	return 0;
+	if (mkfifo(path, 0777) == -1){
+		printf("Error creating FIFO: %s : %s\n", path, strerror(errno));
+		return 0;
+	}
+	if ((fd = open(path, O_WRONLY)) == -1){
+		printf("Error opening FIFO: %s : %s\n", path, strerror(errno));
+		return 0;
+	}
+	return fd;
 }
 
-int main(int argc, char** argv)
-{
-	assert(argc > 3);
 
-	int i;
+int main(int argc, char** argv){
+	assert(argc == 2);
+	f_path = argv[1];
+	if (!reg_signals()) return 0;
+
+	fifo = open_write_fifo(f_path);
+	assert(fifo != 0);
+
 	char line[1024];
-
-	// number of devices == number of arguments (ignore 1st)
-	num_raid1_dev = atoi(argv[1]);
-	num_dev = argc - 2;
-	num_raid0_dev = num_dev / num_raid1_dev;
-	int _dev_fd[num_dev];
-	dev_fd = _dev_fd;
-
-	// open all devices
-	for (i = 0; i < num_dev; ++i) {
-		printf("Opening device %d: %s\n", i, argv[i + 2]);
-		dev_fd[i] = open(argv[i + 2], O_RDWR);
-		assert(dev_fd[i] >= 0);
-	}
-
-	// vars for parsing input line
-	char operation[20];
-	char new_dev[100];
-	int sector;
-	int count;
-
-	// read input lines to get command of type "OP <SECTOR> <COUNT>"
 	while (fgets(line, 1024, stdin) != NULL) {
-		assert(sscanf(line, "%s %d %s", operation, &sector, new_dev) == 3);
-
-		// KILL specified device
-		if (!strcmp(operation, "KILL")) {
-			assert(!close(dev_fd[sector]));
-			dev_fd[sector] = -1;
-		}
-		// REPAIR specified device
-		else if (!strcmp(operation, "REPAIR")) {
-			if (dev_fd[sector] != -1){
-				assert(!close(dev_fd[sector]));
-				dev_fd[sector] = -1;
-			}
-
-			dev_fd[sector] = open(new_dev, O_RDWR);
-			assert(dev_fd[sector] >= 0);
-			restore_data(sector);
-		}
-		// READ / WRITE
-		else {
-			count = atoi(new_dev);
-			do_raid0_rw(operation, sector, count);
+		if (write(fifo, line, strlen(line)) == -1){
+			printf("Error writing to FIFO: %s\n", strerror(errno));
+			return 0;
 		}
 	}
 
-	for (i = 0; i < argc - 1; i++) {
-		if (dev_fd[i] >= 0)
-			assert(!close(dev_fd[i]));
-	}
+	signal_handler(SIGTERM);
+	return 0;
 }
